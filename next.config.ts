06d16aea373d7b415dc4851ce0e3c33f597b266a
박규민 <131234408+kyumin7487@users.noreply.github.com>
2026-02-02 21:26:55 +0900

    config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    output: 'export',
+
+    // 이미지 최적화 끄기
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
